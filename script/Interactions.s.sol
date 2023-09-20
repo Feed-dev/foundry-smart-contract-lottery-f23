3e08548e347df9f6af30eaf04e506bbffb814207
Feed-dev <federico.lubbert@gmail.com>
2023-09-20 09:43:14 +0200

    Lesson 9 Interactions - create subId

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
@@ -17,8 +18,12 @@ contract CreateSubscription is Script {
     ) public returns (uint64) {
         console.log("Creating subscription on chainId: ", block.chainid);
         vm.startBroadcast();
-
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
+            .createSubscription();
         vm.stopBroadcast();
+        console.log("Subscription created with id: ", subId);
+        console.log("Please update subscriptionId in HelperConfig.s.sol");
+        return subId;
     }
 
     function run() external returns (uint64) {
