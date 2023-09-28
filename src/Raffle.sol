0674157c20fe70d8950be929b01326a367e73106
Feed-dev <federico.lubbert@gmail.com>
2023-09-28 21:20:42 +0200

    Lesson 9 one big test

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -167,4 +167,16 @@ contract Raffle is VRFConsumerBaseV2 {
     function getPlayer(uint256 indexOfPlayer) external view returns (address) {
         return s_players[indexOfPlayer];
     }
+
+    function getRecentWinner() external view returns (address) {
+        return s_recentWinner;
+    }
+
+    function getNumberOfPlayers() external view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLastTimeStamp() public view returns (uint256) {
+        return s_lastTimestamp;
+    }
 }
